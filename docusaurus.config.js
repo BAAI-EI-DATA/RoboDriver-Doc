fd204d1c20007f6ee8852c99ad57d4441b713fb0
Xiang Yang <Ryu-Yang@qq.com>
2025-11-21 17:14:19 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://BAAI-EI-DATA.github.io/',
+  url: 'https:///baai-ei-data.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/website-test/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
